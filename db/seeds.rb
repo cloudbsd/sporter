f0351932d067c3695c1f45abae76444a53ccee0c
Qi Li <cloudbsd@gmail.com>
2014-03-15 18:14:32 +0800

    show operated date in all transactions pages.

--- db/seeds.rb
+++ db/seeds.rb
@@ -128,10 +128,10 @@ def import_transactions
     while row = file.gets
       row.strip!
       names = row.split(',')
-      name, amount, operated_at = names[0].strip, names[1].strip.to_f, names[2].strip
+      name, action, amount, operated_at = names[0].strip, names[1].strip, names[2].strip.to_f, names[3].strip
       user = User.find_by(name: name)
-      action = (amount > 0.0) ? 'deposit' : 'withdraw'
-      amount = -amount if action == "withdraw"
+    # action = (amount > 0.0) ? 'deposit' : 'withdraw'
+    # amount = -amount if action == "withdraw"
     # txn = Transaction.create!(user_id: user.id, action: action, amount: amount, operated_at: DateTime.now)
       txn = Transaction.create!(user_id: user.id, action: action, amount: amount, operated_at: operated_at)
       print_content "imported transaction: #{txn.user.name} #{txn.action} #{txn.amount}"
