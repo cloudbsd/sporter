9020a03b4055780042621e520927ae1141debc5c
Qi Li <cloudbsd@gmail.com>
2014-03-25 04:31:03 +0800

    new header.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,8 @@
 module UsersHelper
   def header_link_to cname, aname, title, path
     if controller_name == cname and (aname.nil? or params[:action] == aname)
-      content_tag :li, class: 'active' do
+    # content_tag :li, class: 'active' do
+      content_tag :li do
         link_to title, path
       end
     else
